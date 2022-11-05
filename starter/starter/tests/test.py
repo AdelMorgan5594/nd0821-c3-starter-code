aa44cfda71f46c4f364a48f194a0814090b0b2c9
Morgan <morgan@158429s0.speedport.ip>
2022-11-05 21:57:45 +0100

    adding tests

---
+++ starter/starter/tests/test.py
@@ -0,0 +1,27 @@
+import pytest
+import pandas as pd
+from ml.data import process_data
+
+
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+
+
+@pytest.fixture
+def data():
+    df = pd.read_csv('starter/data/census_clean.csv')
+    return df
+
+def test_process_data(data):
+    X, y, encoder, lb = process_data(data, categorical_features=cat_features, label="salary", training=True)
+    assert X.shape == (32561, 107)
+    assert y.shape == (32561,)
