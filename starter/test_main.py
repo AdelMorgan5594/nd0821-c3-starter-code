8800131e6ee280dd92041535702a7b5ff59618b0
Morgan <morgan@158429s0.speedport.ip>
2022-11-15 22:24:12 +0100

    remove word inference

--- starter/test_main.py
+++ starter/test_main.py
@@ -29,7 +29,7 @@ def test_less_than_or_equal_50k():
         "hours_per_week": "50",
         "native_country": "Yugoslavia",
     } 
-    r = client.post("/inference/", json=data)
+    r = client.post("/", json=data)
     #assert r.status_code == 200
     assert r.json() == {"salary": "<=50k"}
 
@@ -51,6 +51,6 @@ def test_morethan_50k():
         "hours_per_week": "55",
         "native_country": " United-States",
     } 
-    r = client.post("/inference/", json=data)
+    r = client.post("/", json=data)
     #assert r.status_code == 200
     assert r.json() == {"salary": ">50k"}
