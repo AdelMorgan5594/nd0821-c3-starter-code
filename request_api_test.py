6f9a8768a58b1a12eb912c6f6d4411e4960e61c7
Morgan <morgan@158429s0.speedport.ip>
2022-11-19 22:58:52 +0100

    adding request to the app

---
+++ request_api_test.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+data = {
+        "age": 45,
+        "workclass": "Private",
+        "fnlgt": 368561,
+        "education": "HS-grad",
+        "education-num": 9,
+        "marital-status": "Married-civ-spouse",
+        "occupation": "Exec-managerial",
+        "relationship": "Husband",
+        "race": " White",
+        "sex": "Male",
+        "hours-per-week": "55",
+        "native-country": " United-States",
+    } 
+
+response = requests.post("https://thirdprojectudacity.herokuapp.com/", json=data)
+print(response.status_code)
+print(response.json())
